697503ac53a6ce42a9b605dd94d4962d1dab3baf
zhigunovsemyon <zhigunovsemen@gmail.com>
2024-04-27 11:55:05 +0300

    Создана библиотека для работы со стеком строк

---
+++ src/ListLib.c
@@ -0,0 +1,45 @@
+#include "ListLib.h"
+
+uint8_t PushElement(TextList **list, const char *text)
+{
+	//Выделение памяти под новый элемент, проверка, удалось ли
+	TextList *new = (TextList *)malloc(sizeof(TextList) + sizeof(char) * strlen(text));
+	if (!new)
+		return ERR_MALLOC;
+
+	new->next = *list;				//Сохранение адреса старого элемента в буфер
+	new->text = (char *)(new + 1);	//Присвоение адреса текстового поля
+	strcpy(new->text, text);		//Копирование текста в поле
+	*list = new;					//Запись нового адреса в список
+
+	return ERR_NO;
+}
+
+void PrintList(TextList *list)
+{
+	if (list)	//Вывод осуществляется только если список существует
+	{
+		do
+			printf("%s", list->text);	//Вывод текущей строки
+		while ((list = list->next));	//Переключение на новую строку	
+		//Если следующей строкой оказался NULL, цикл завершается
+	}
+	putchar('\n');
+}
+
+void RemoveTopElement(TextList **list)
+{
+	TextList *tmp = (*list)->next;	//Сохранение следующего элемента
+	free(*list);					//Удаление текущего элемента
+	*list = tmp;					//Подстановка следующего элемента на место старого
+}
+
+void RemoveList(TextList *list)
+{
+	while (list)	//Пока указатель на список не равен NULL
+	{
+		RemoveTopElement(&list);	//Удаление верхнего элемента
+		if (list)					//Если он не был последним, то переключение на следующий
+			list = list->next;
+	}
+}
---
+++ src/ListLib.h
@@ -0,0 +1,32 @@
+#include <stdint.h>
+#include <malloc.h>
+#include <string.h>
+
+enum Erorrs
+{
+	ERR_NO,		//Отсутствие ошибок
+	ERR_MALLOC,	//Ошибка выделения памяти
+	ERR_BADCALL	//Неправильный вызов программы
+};
+
+//LIFO список строк
+typedef struct TextList TextList;
+struct TextList
+{
+	char *text;		//Текстовое поле
+	TextList *next;	//Указатель на следующий элемент
+};
+
+/*Добавление новой строки text по адресу указателя на список list
+Возврат ERR_NO при отсутствии ошибок, либо ERR_MALLOC при нехватке памяти*/
+uint8_t PushElement(TextList **list, const char *text);
+
+//Вывод всего списка list без разделителей между элементами
+void PrintList(TextList *list);
+
+//Удаление верхнего элемента по адресу указателя на список list
+void RemoveTopElement(TextList **list);
+
+//Удаление всего списка list
+void RemoveList(TextList *list);
+
